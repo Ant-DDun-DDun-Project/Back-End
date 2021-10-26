d96d985aba2fb1c29f42f1d5688862dca975a488
yunjaejo <1a616@naver.com>
2021-10-26 17:21:28 +0900

    [fix] 테스트코드 버그픽스 : 조윤재

--- __tests__/login.test.js
+++ __tests__/login.test.js
@@ -1,25 +1,35 @@
 jest.mock('../models/users');
 const User = require('../models/users');
+const { login } = require('../controllers/login');
+
+const mockdb = {
+  id: 1,
+  userId: 'a-sd_f',
+  pw: '$2b$10$LNSjqODSOQNv0tJC2b0qne/gLwxFKI3WaFTiGuJETnGSCLgfwuvx.',
+  nickname: '황창환',
+  ageGroup: '20',
+  exp: 0,
+};
 
 describe('로그인', () => {
   const res = {
     status: jest.fn(() => res),
     json: jest.fn(),
+    cookie: jest.fn(),
   };
   const next = jest.fn();
   test('로그인에 성공하면 response로 success:true와 token을 보낸다.', async () => {
     const req = {
       body: {
         userId: 'a-sd_f',
-        pw: 'zxcv',
+        pw: 'zxcv1234!@',
       },
     };
-    await User.findOne.mockReturnValue(null);
+    await User.findOne.mockReturnValue(mockdb);
     await login(req, res, next);
     expect(res.status).toBeCalledWith(200);
     expect(res.json).toBeCalledWith({
       success: true,
-      token,
     });
   });
   test('로그인에 실패하면 response로 success:false를 보낸다.', async () => {
@@ -29,7 +39,7 @@ describe('로그인', () => {
         pw: 'zxcv',
       },
     };
-    await User.findOne.mockReturnValue(null);
+    await User.findOne.mockReturnValue(mockdb);
     await login(req, res, next);
     expect(res.status).toBeCalledWith(400);
     expect(res.json).toBeCalledWith({
@@ -37,9 +47,15 @@ describe('로그인', () => {
     });
   });
   test('DB에서 에러가 날 경우 에러핸들러로 넘긴다.', async () => {
-    const error = 'DB에러';
-    await User.findOne.mockRejectedValue(error);
+    const req = {
+      body: {
+        userId: 'a-sd_f',
+        pw: 'zxcv',
+      },
+    };
+    const err = 'DB에러';
+    await User.findOne.mockRejectedValue(err);
     await login(req, res, next);
-    expect(next).toBeCalledWith(error);
+    expect(next).toBeCalledWith(err);
   });
 });
--- controllers/login.js
+++ controllers/login.js
@@ -4,22 +4,27 @@ require('dotenv').config();
 const { createToken } = require('./utils/create-token.js');
 
 exports.login = async (req, res, next) => {
-  const { userId, pw } = req.body;
-  const userData = await User.findOne({
-    where: {
-      userId,
-    },
-  });
-  const pwCheck = bcrypt.compareSync(pw, userData.pw);
-  if (!userData || !pwCheck) {
-    res.status(400).json({
-      success: false,
+  try {
+    const { userId, pw } = req.body;
+    const userData = await User.findOne({
+      where: {
+        userId,
+      },
     });
-  } else {
-    const token = createToken(userData.id);
-    res.status(200).json({
-      success: true,
-      token,
-    });
+    const pwCheck = bcrypt.compareSync(pw, userData.pw);
+    if (!userData || !pwCheck) {
+      res.status(400).json({
+        success: false,
+      });
+    } else {
+      const token = createToken(userData.id);
+      res.cookie('user', token, { httpOnly: true, maxAge: 60 * 60 * 24 * 1000 });
+      res.status(200).json({
+        success: true,
+      });
+    }
+  } catch (err) {
+    console.log('로그인 시 에러발생', err);
+    next(err);
   }
 };
