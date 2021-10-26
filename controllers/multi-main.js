4fd82be5be4fdedb345b66833c57c82575d94935
jeangho <jeangho293@naver.com>
2021-10-27 02:31:58 +0900

    [feat] 객관식 페이지 틀 추가 : 김정호

---
+++ controllers/multi-main.js
@@ -0,0 +1,10 @@
+const Multi = require('../models/multi');
+
+exports.mainMulti = async (req, res, next) => {
+  try{
+    
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+}
--- routes/multi.js
+++ routes/multi.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { postMulti } = require('../controllers/multi-post');
+const { mainMulti } = require('../controllers/multi-main');
 const { postComment } = require('../controllers/comment');
 
+router.get('/', mainMulti);
 router.post('/:multi_id/comment', postComment); // 댓글 
 router.post('/', postMulti);  // 객관식 게시글 작성
 
