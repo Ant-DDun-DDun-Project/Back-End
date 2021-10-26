f73ce9583d5ce37a35e2189524390207aca8a9ea
yunjaejo <1a616@naver.com>
2021-10-27 00:08:51 +0900

    [fix] joi 컨트롤러 오류수정 : 조윤재

--- controllers/joi.js
+++ controllers/joi.js
@@ -1,7 +1,8 @@
 const Joi = require('joi');
 
 module.exports = {
-  signUpSchema: Joi.object({  // 회원가입 Schema
+  signUpSchema: Joi.object({
+    // 회원가입 Schema
     userId: Joi.string()
       .required()
       .min(5)
@@ -17,30 +18,27 @@ module.exports = {
     ageGroup: Joi.number().required(),
   }),
 
-  eitherSchema: Joi.object({  // 찬반투표 게시물 Schema
+  eitherSchema: Joi.object({
+    // 찬반투표 게시물 Schema
     title: Joi.string().required(),
     contentA: Joi.string().required(),
     contentB: Joi.string().required(),
+  }),
 
-  multiSchema: Joi.object({   // 객관식 게시물 Schema
-    title: Joi.string()
-      .required().min(1),
-    description: Joi.string()
-      .required().min(1),
-    contentA: Joi.string()
-      .required(),
-    contentB: Joi.string()
-      .required(),
-    contentC: Joi.string()
-      .allow(null),
-    contentD: Joi.string()
-      .allow(null),
-    contentE: Joi.string()
-      .allow(null),
-    date: Joi.string()
-      .required()
-  })
-  postCommentSchema: Joi.object({ // 댓글 작성 Schema
+  multiSchema: Joi.object({
+    // 객관식 게시물 Schema
+    title: Joi.string().required().min(1),
+    description: Joi.string().required().min(1),
+    contentA: Joi.string().required(),
+    contentB: Joi.string().required(),
+    contentC: Joi.string().allow(null),
+    contentD: Joi.string().allow(null),
+    contentE: Joi.string().allow(null),
+    date: Joi.string().required(),
+  }),
+
+  postCommentSchema: Joi.object({
+    // 댓글 작성 Schema
     comment: Joi.string().required().min(1),
 
     date: Joi.string().required(),
