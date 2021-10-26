d0a503db35a52684c73cee51bcdb258cc3e86cd9
jeangho <jeangho293@naver.com>
2021-10-26 22:02:20 +0900

    [style] 해당 기능에 대한 주석 추가 : 김정호

--- controllers/multi-post.js
+++ controllers/multi-post.js
@@ -1,6 +1,7 @@
 const MultiPost = require('../models/multi');
 const Joi = require('joi');
 
+// req.body 에 대한 Joi 검증 기능
 async function MultiValidation(req) {
   const MultiSchema = Joi.object({
     title: Joi.string().required(),
@@ -12,18 +13,18 @@ async function MultiValidation(req) {
     contentF: Joi.string(),
     contentE: Joi.string(),
     date: Joi.string().required(),
-  });
+  });   // Joi Schema 정의
   const {
     title, description, contentA, contentB,
     contentC, contentD, contentE, date,
-  } = await MultiSchema.validateAsync(req.body);
+  } = await MultiSchema.validateAsync(req.body);  // Joi 검증
   return {
     title, description, contentA, contentB,
     contentC, contentD, contentE, date,
   };
 }
 
-// 게시글 작성에 대한
+// 게시글 작성에 대한 기능
 exports.postMulti = async (req, res, next) => {
   const {
     title, description, contentA, contentB,
@@ -36,7 +37,7 @@ exports.postMulti = async (req, res, next) => {
       user, title, description, contentA, contentB,
       contentC, contentD, contentE, date
     });
-    res.status(200).json({ success: true });
+    res.status(200).json({ success: true });  // 게시글 작성 성공시 { success: true } 전송
   } catch (err) {
     console.error(err);
     next(err);
