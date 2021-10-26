ca7721671e71a4b68b765c9ea261d37f165634db
jeangho <jeangho293@naver.com>
2021-10-26 15:13:17 +0900

    [feat] signup-duplication 기능 추가

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const signup = require('../controllers/signup');
+const { CheckDuplicatedId, CheckDuplicatedNick } = require('../controllers/signup-duplication');
 
 router.post('/signup', signup);
+router.post('/signup/id', CheckDuplicatedId);
+router.post('/signup/nick', CheckDuplicatedNick);
 
 module.exports = router;
