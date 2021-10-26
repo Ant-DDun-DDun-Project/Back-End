733d4f52a8a8d86cd221be0dc30677d40f80506b
CH_Hwang <hch950627@naver.com>
2021-10-26 15:43:39 +0900

    [fix] 로그인 및 테스트 코드 수정

--- __tests__/password-validation.test.js
+++ __tests__/password-validation.test.js
@@ -1,10 +1,14 @@
-const validatePassword = require('../controllers/utils/password-validation');
+const {
+  validatePassword,
+} = require('../controllers/utils/password-validation');
 
 test(`비밀번호를 입력했을때 
         password와 passwordCheck가 일치할 때,
         true를 반환한다.`, () => {
   expect(validatePassword('aaAA11!@', 'aaAA11!@')).toEqual(true);
-  expect(validatePassword('aaaaaAAAAA!@#$%^')).toEqual(true);
+  expect(validatePassword('aaaaaAAAAA!@#$%^', 'aaaaaAAAAA!@#$%^')).toEqual(
+    true
+  );
 });
 
 test('비밀번호를 입력했을때 password와 passwordcheck가 일치하지 않으면 false를 반환한다.', () => {
--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const signup = require('../controllers/signup');
+const { signup } = require('../controllers/signup');
 
 router.post('/signup', signup);
 
