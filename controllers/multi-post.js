ca5070815256739e7a47c6743597e73bc6e0df9c
jeangho <jeangho293@naver.com>
2021-10-26 22:31:45 +0900

    [fix] Joi allow 옵션 추가 및 try-catch 안으로 삽입 : 김정호

--- controllers/multi-post.js
+++ controllers/multi-post.js
@@ -8,10 +8,9 @@ async function MultiValidation(req) {
     description: Joi.string().required(),
     contentA: Joi.string().required(),
     contentB: Joi.string().required(),
-    contentC: Joi.string(),
-    contentD: Joi.string(),
-    contentF: Joi.string(),
-    contentE: Joi.string(),
+    contentC: Joi.string().allow(null),
+    contentD: Joi.string().allow(null),
+    contentE: Joi.string().allow(null),
     date: Joi.string().required(),
   });   // Joi Schema 정의
   const {
@@ -26,12 +25,13 @@ async function MultiValidation(req) {
 
 // 게시글 작성에 대한 기능
 exports.postMulti = async (req, res, next) => {
-  const {
-    title, description, contentA, contentB,
-    contentC, contentD, contentE, date,
-  } = await MultiValidation(req);   // Joi Schema 검증 절차
   try {
+    const {
+      title, description, contentA, contentB,
+      contentC, contentD, contentE, date,
+    } = await MultiValidation(req);   // Joi Schema 검증 절차
     const user = res.locals.user;   // 로그인한 유저는 user 아이디를 갖는다.
+    // const user = 'jeangho293';   // Todo --> 사용자 인증 미들웨어를 지나온다면 제거해야하는 부분
     // 객관식 게시물 DB 저장
     await MultiPost.create({
       user, title, description, contentA, contentB,
