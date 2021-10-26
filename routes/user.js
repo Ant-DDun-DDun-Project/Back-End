73721531af7b8ae03a2bdccee6b6401a63b18e82
yunjaejo <1a616@naver.com>
2021-10-26 16:02:20 +0900

    [style] user.js 구조분해할당

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { signup } = require('../controllers/signup');
-const { login } = require('../controllers/login');
+const { signup, login } = require('../controllers/');
 
 router.post('/signup', signup);
 router.post('/login', login);
