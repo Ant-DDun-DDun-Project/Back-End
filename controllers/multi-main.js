0a46dd25f68a53b04f0afddbe659f7b01df671d2
jeangho <jeangho293@naver.com>
2021-10-27 14:55:31 +0900

    [fix] 객관식 메인페이지 버그 수정 : 김정호

--- controllers/multi-main.js
+++ controllers/multi-main.js
@@ -6,35 +6,33 @@ const Comment = require('../models/comments');
 
 exports.mainMulti = async (req, res, next) => {
   try {
-    const user = 1;
+    const user = 3;                // Todo --> 사용자 인증 미들웨어 구현 시 삭제
     // const user = res.locals.user // Todo --> 사용자 인증 미들웨어 구현 시 활성화
     const multi = await Multi.findAll({
       attributes: [
         'multiId', 'user', 'title', 'description',
         'date', 'editedDate', 'completed', 'likeCnt',
-        //[Sequelize.fn('COUNT', Sequelize.col('comments.id')), 'commentCnt'],
       ],
       include: [
-        {
+        { // 현재 작성글에 대한 Comment 의 전체 개수
           model: Comment,
           attributes: [[Sequelize.fn('COUNT', Sequelize.col('id')), 'commentCnt']],
-          separate: true,
+          separate: true, // 여러 join 에 대해서 각각의 Query 문을 실행한다.
         },
-        {
+        { // 현재 작성글에 대한 Vote 를 로그인한 유저가 했는지 안했는지에 대한 확인
           model: Vote,
           where: { user },
           attributes: [['vote', 'voted']],
           separate: true,
         },
-        {
+        { // 현재 작성글에 대한 Like 를 로그인한 유저가 했는지 안했는지에 대한 확인
           model: Like,
           where: { user },
           attributes: [['user', 'liked']],
           separate: true,
         },
       ],
-      separate: true,
-      order: [['date', 'DESC']],
+      order: [['date', 'DESC']],  // 최신 날짜 순으로 정렬
     });
     res.json({ success: 'true', multi });
   } catch (err) {
