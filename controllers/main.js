745e200116a209d1032eca7fcc70b80415ca66ba
CH_Hwang <hch950627@naver.com>
2021-11-12 20:52:52 +0900

    [docs] 주석작업 1차 완료

--- controllers/main.js
+++ controllers/main.js
@@ -8,12 +8,13 @@ module.exports = {
   getMain: async (req, res, next) => {
     try {
       const [either, multi, [eitherNum, multiNum], attendNum] = await Promise.all([
+        //Promise.all로 각각의 데이터들(찬반투표 포스팅, 객관식 포스팅, 찬반투표 포스팅갯수, 객관식 포스팅갯수, 참여자수)를 병렬적으로 받아온다.
         sequelize.query(mainQuery.getMainForEither(), { type: sequelize.QueryTypes.SELECT }),
         sequelize.query(mainQuery.getMainForMulti(), { type: sequelize.QueryTypes.SELECT }),
         countPosting(),
         countAttend(),
       ]);
-      const postingNum = eitherNum + multiNum;
+      const postingNum = eitherNum + multiNum; //포스팅 전체 갯수 = 찬반투표 포스팅갯수 + 객관식 포스팅 갯수
       res.status(200).json({
         success: true,
         either,
@@ -22,7 +23,7 @@ module.exports = {
         multiNum,
         eitherNum,
         attendNum,
-      });
+      }); //status code 200, success:true, 각각의 데이터들(찬반투표 포스팅, 객관식 포스팅, 전체 포스팅 갯수, 찬반투표 포스팅갯수, 객관식 포스팅갯수, 참여자수)
     } catch (err) {
       next(err);
     }
--- controllers/utils/create-token.js
+++ controllers/utils/create-token.js
@@ -4,6 +4,6 @@ module.exports = {
   //토큰 생성
   createToken(id) {
     //user의 id값(로그인 시 필요한 아이디)을 인자로 받음
-    return jwt.sign({ id }, process.env.SECRET_KEY, { expiresIn: '72h' }); //유효시간이 72시간인 jwt 생성
+    return jwt.sign({ id }, process.env.SECRET_KEY, { expiresIn: '72h' }); //유효시간이 72시간인 jwt 생성하여 반환한다.
   },
 };
