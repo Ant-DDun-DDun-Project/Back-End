fb3c927789da2fe8e71cbb4a9edf3126a1721d68
CH_Hwang <hch950627@naver.com>
2021-11-17 17:38:07 +0900

    [edit] 기존 프로젝트 폴더 백업 후 삭제

--- routes/main.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getMain } = require('../controllers/main');
-const { authForGuest } = require('../middlewares/auth');
-
-router.get('/', authForGuest, getMain); //메인 뷰
-
-module.exports = router;
