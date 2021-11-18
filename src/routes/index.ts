589308b440445d1644a36bf6543165ad5d89af26
Yunjaejo <1a616@naver.com>
2021-11-18 11:30:54 +0900

    [mig] routes/index.ts 테스트 제거

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,4 @@
 import * as express from 'express';
 const router = express.Router();
 
-import { Request, Response, NextFunction } from 'express';
-router.get('/', (req: Request, res: Response, next: NextFunction) => {
-  res.send('hi');
-});
-
 export default router;
