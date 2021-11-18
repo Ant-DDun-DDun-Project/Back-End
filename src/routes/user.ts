b2f7136dc1a5886c75036f15817fa9bb6d5be32f
jeangho <jeangho293@naver.com>
2021-11-18 13:04:00 +0900

    [mig][style] 로그인 migration (js 파일 삭제): 김정호

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,6 +1,6 @@
 import * as express from 'express';
 const router = express.Router();
-import userControllers from '../controllers/user'
+import userControllers from '../controllers/user';
 
 router.post('login', userControllers.login); // 로그인
 router.get('/login', userControllers.checkLoginStatus); // 로그인 상태 확인
