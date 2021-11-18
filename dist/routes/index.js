9c4be78f07beb03bf4fdb0226039b7c66917a38c
jeangho <jeangho293@naver.com>
2021-11-18 13:02:12 +0900

    [mig] 로그인 migration (js 파일 삭제): 김정호

--- dist/routes/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express = require("express");
-const router = express.Router();
-router.get('/', (req, res, next) => {
-    res.send('hi');
-});
-exports.default = router;
-//# sourceMappingURL=index.js.map
