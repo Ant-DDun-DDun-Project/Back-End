0d789412ea9823d38d614d6defb58edffb268c46
jeangho <jeangho293@naver.com>
2021-11-18 15:53:19 +0900

    [mig] 사용자 인증 미들웨어 migration: 김정호

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,35 @@
+import * as jwt from 'jsonwebtoken';
+import 'dotenv/config';
+import { Request, Response, NextFunction } from 'express';
+
+// 게스트 허용 인증 미들웨어
+export function authForGuest(req: Request, res: Response, next: NextFunction) {
+  try {
+    const token: string = req.cookies.user;
+    if (!token) {
+      res.locals.user = 13;
+      next();
+    } else {
+      const id = jwt.verify(token, process.env.SECRET_KEY) as jwt.JwtPayload;
+      res.locals.user = id;
+      next();
+    }
+  } catch (err) {
+    next(err);
+  }
+}
+// 사용자 인증 미들웨어
+export function auth(req: Request, res: Response, next: NextFunction) {
+  try {
+    const token: string = req.cookies.user;
+    if (!token) {
+      res.status(401).json({ success: false });
+    } else {
+      const id = jwt.verify(token, process.env.SECRET_KEY) as jwt.JwtPayload;
+      res.locals.user = id;
+      next();
+    }
+  } catch (err) {
+    next(err);
+  }
+}
