de6cb3600d6423d91578e1ff10b423b4eb19d6e7
Yunjae Jo <73681656+Yunjaejo@users.noreply.github.com>
2021-11-19 15:03:21 +0900

    Delete dist directory

--- dist/server1.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = require("./app");
-const port = process.env.PORT;
-//server
-app_1.default.listen(port, () => {
-    console.log(`listening at http://localhost:${port}`);
-});
-//# sourceMappingURL=server1.js.map
