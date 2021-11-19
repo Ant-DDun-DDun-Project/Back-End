afe515a094adea2a2e2324cd0a9bc7f5aee9d163
CH_Hwang <hch950627@naver.com>
2021-11-19 15:14:08 +0900

    [edit] Joi validation 글자수 제한
    [edit] Joi validation 글자수 제한

--- controllers/joi.js
+++ controllers/joi.js
@@ -24,48 +24,48 @@ module.exports = {
   }),
   // 찬반투표 게시물 Schema
   eitherSchema: Joi.object({
-    title: Joi.string().required(),
-    contentA: Joi.string().required(),
-    contentB: Joi.string().required(),
+    title: Joi.string().required().max(30),
+    contentA: Joi.string().required().max(30),
+    contentB: Joi.string().required().max(30),
     date: Joi.string().required(),
   }),
   // 객관식 게시물 Schema
   multiSchema: Joi.object({
-    title: Joi.string().required().min(1),
+    title: Joi.string().required().max(30),
     description: Joi.string().required().min(1),
-    contentA: Joi.string().required(),
-    contentB: Joi.string().required(),
-    contentC: Joi.string().allow(null),
-    contentD: Joi.string().allow(null),
-    contentE: Joi.string().allow(null),
+    contentA: Joi.string().required().max(30),
+    contentB: Joi.string().required().max(30),
+    contentC: Joi.string().allow(null).max(30),
+    contentD: Joi.string().allow(null).max(30),
+    contentE: Joi.string().allow(null).max(30),
     date: Joi.string().required(),
   }),
   // 댓글 작성 Schema
   postCommentSchema: Joi.object({
-    comment: Joi.string().required().min(1),
+    comment: Joi.string().required().min(1).max(1000),
     date: Joi.string().required(),
   }),
   // 댓글 수정 Schema
   editCommentSchema: Joi.object({
-    comment: Joi.string().required(),
+    comment: Joi.string().required().max(1000),
     editedDate: Joi.string().required(),
   }),
   // 객관식 게시물 수정 Schema
   editMultiSchema: Joi.object({
-    title: Joi.string().required().min(1),
+    title: Joi.string().required().min(1).max(30),
     description: Joi.string().required().min(1),
-    contentA: Joi.string().required(),
-    contentB: Joi.string().required(),
-    contentC: Joi.string().allow(null),
-    contentD: Joi.string().allow(null),
-    contentE: Joi.string().allow(null),
+    contentA: Joi.string().required().max(30),
+    contentB: Joi.string().required().max(30),
+    contentC: Joi.string().allow(null).max(30),
+    contentD: Joi.string().allow(null).max(30),
+    contentE: Joi.string().allow(null).max(30),
     editedDate: Joi.string().allow(null),
   }),
   // 찬반투표 게시물 수정 Schema
   editEitherSchema: Joi.object({
-    title: Joi.string().required().min(1),
-    contentA: Joi.string().required().min(1),
-    contentB: Joi.string().required().min(1),
+    title: Joi.string().required().min(1).max(30),
+    contentA: Joi.string().required().min(1).max(30),
+    contentB: Joi.string().required().min(1).max(30),
     editedDate: Joi.string().required(),
   }),
   // 아이디 중복 체크 Schema
