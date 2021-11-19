1b9443c379dacd0e99454ce7ed8a8009223a9ad8
Yunjaejo <1a616@naver.com>
2021-11-19 15:03:37 +0900

    [edit] 회원가입 로직 수정 : 조윤재

--- dist/routes/main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express = require("express");
-const main_1 = require("../controllers/main");
-const router = express.Router();
-router.get('/', main_1.default.getMain);
-exports.default = router;
-//# sourceMappingURL=main.js.map
