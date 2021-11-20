bd4d4dd5698ff70d7346d25c0fe0aed4ae397513
CH_Hwang <hch950627@naver.com>
2021-11-20 19:59:24 +0900

    [edit] delayMs 삭제

--- middlewares/rate-limit.js
+++ middlewares/rate-limit.js
@@ -3,7 +3,6 @@ const limit = require('express-rate-limit');
 exports.limiter = new limit({
   windowMs: 1000, //이 시간동안
   max: 15, //최대횟수
-  delayMs: 0, //요청간 간격
   handler(req, res) {
     //어겼을 경우
     res.status(this.statusCode).json({
