35b7e2296fa4b3e63c9a207fac00d27d8a8de3aa
jeangho <jeangho293@naver.com>
2021-11-25 12:15:49 +0900

    [style] 주석 처리: 김정호

--- src/middlewares/DAU.ts
+++ src/middlewares/DAU.ts
@@ -8,7 +8,7 @@ import { client } from './redis';
 // 메인 페이지 방문자 카운팅
 export const mainVisitor = async (req: Request, res: Response, next: NextFunction) => {
   const clientIp: string = requestIp.getClientIp(req);  // client의 IP 주소 가져오기
-  console.log(clientIp);
+  console.log(`접속 IP: clientIp`);
   const now = moment();
   const end = moment(moment().format('YYYY-MM-DD 23:59:59'));
   const duration = moment.duration(end.diff(now));
